bfdd2097dd3f7f2aa9043f43807e4e1c0bbbe5b7
Roy de Wildt <roydewildt@gmail.com>
2023-11-11 10:52:47 +0100

    Storing exercise logic

--- exercise.go
+++ exercise.go
@@ -26,10 +26,12 @@ var (
 
 var (
 	ErrNoIDProvided     = errors.New("no exercise id provided")
-	ErrInvalidIdFormat  = errors.New("id is incorrectly formatted")
+	ErrInvalidIdFormat  = errors.New("id incorrectly formatted")
 	ErrExerciseNotFound = errors.New("exercise not found")
 )
 
+// ExerciseRetreiver is an interface for retreiving an Exercise
+// from an exercises repository
 type ExerciseRetreiver interface {
 	ExerciseByID(uuid.UUID) (Exercise, error)
 }
@@ -39,31 +41,31 @@ type ExerciseRetreiver interface {
 func (s *Server) HandleSingleExercise(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "exerciseID")
 
-	s.logger.Debug("exercise %s requested", "id", id, "path", r.URL.Path)
+	s.logger.Debug("new single exercise request", "id", id, "path", r.URL.Path)
 
 	e, err := FetchSingleExerciseJSON(s.exercises, id)
 	if err != nil {
-		msg := fmt.Errorf("exercise with id %s: %w", id, err).Error()
+		msg := fmt.Errorf("exercise retrieval error: %w", err).Error()
 		s.logger.Error(msg)
 		http.Error(w, msg, http.StatusInternalServerError)
-	}
-
-	payload, err := json.Marshal(&e)
-	if err != nil {
-		msg := fmt.Errorf("marshal exercise %s: %w", id, err).Error()
-		s.logger.Error(msg)
-		http.Error(w, msg, http.StatusInternalServerError)
+		return
 	}
 
 	w.Header().Set("Content-Type", jsonapi.MediaType)
+
 	w.WriteHeader(http.StatusOK)
-	if _, err := w.Write(payload); err != nil {
-		msg := fmt.Errorf("writing response: %w", err).Error()
+
+	if _, err := w.Write(e); err != nil {
+		msg := fmt.Errorf("exercise response error: %w", err).Error()
 		s.logger.Error(msg)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 }
 
+// FetchSingleExerciseJSON takes a exercise repository and an id and returns
+// an exercise in JSON format. the id must be a valid UUID or ErrInvalidIdFormat is returned
+// if no exercise with that id is found an ErrExerciseNotFound is returned
 func FetchSingleExerciseJSON(exercises ExerciseRetreiver, id string) ([]byte, error) {
 	if id == "" {
 		return emptyJSON, ErrNoIDProvided
@@ -86,4 +88,42 @@ func FetchSingleExerciseJSON(exercises ExerciseRetreiver, id string) ([]byte, er
 	}
 
 	return payload, nil
+}
+
+var (
+	ErrEmptyExercise = errors.New("empty exercise")
+	ErrInvalidJSON   = errors.New("invalid json")
+)
+
+// ExerciseStorer is an interface for storing Exercises in
+// an exercises repository
+type ExerciseStorer interface {
+	Exists(uuid.UUID) bool
+	StoreExercise(Exercise) (uuid.UUID, error)
+}
+
+// StoreExerciseJSON stores an Exercise formatted as JSON in the exercises
+// repository provided by the ExerciseStorer. If the JSON contains an ID
+// it will overwrite, or throws an error if the ID doesn't exist
+func StoreExerciseJSON(exercises ExerciseStorer, exerciseJSON []byte) (uuid.UUID, error) {
+	var e Exercise
+
+	// also throws the error if the id is of an invalid uuid lenght
+	if err := json.Unmarshal(exerciseJSON, &e); err != nil {
+		return uuid.Nil, ErrInvalidJSON
+	}
+
+	if (e == Exercise{}) {
+		return uuid.Nil, ErrEmptyExercise
+	}
+
+	if e.ID != uuid.Nil && !exercises.Exists(e.ID) {
+		return uuid.Nil, ErrExerciseNotFound
+	}
+
+	if e.ID == uuid.Nil {
+		e.ID = uuid.New()
+	}
+
+	return exercises.StoreExercise(e)
 }
--- exercise_test.go
+++ exercise_test.go
@@ -28,8 +28,20 @@ func (xs *testExercises) ExerciseByID(id uuid.UUID) (Exercise, error) {
 	if _, ok := xs.mocks[id.String()]; !ok {
 		return Exercise{}, ErrExerciseNotFound
 	}
-
 	return xs.mocks[id.String()], nil
+}
+
+// StoreExercise stores an Exercise in the mocks map, when ID already exists it
+// overwrites the exercise
+func (xs *testExercises) StoreExercise(e Exercise) (uuid.UUID, error) {
+	xs.mocks[e.ID.String()] = e
+	return e.ID, nil
+}
+
+// Exists returns true if id is in mocks
+func (xs *testExercises) Exists(id uuid.UUID) bool {
+	_, ok := xs.mocks[id.String()]
+	return ok
 }
 
 func TestFetchSingleExerciseJSON(t *testing.T) {
@@ -47,7 +59,7 @@ func TestFetchSingleExerciseJSON(t *testing.T) {
 		{"noId", "", emptyJSON, ErrNoIDProvided},
 		{"InvalidIdFormat", "1234", emptyJSON, ErrInvalidIdFormat},
 		{"IdNotFound", uuid.Nil.String(), emptyJSON, ErrExerciseNotFound},
-		{"withID", e1.ID.String(), e1JSON, nil},
+		{"existingID", e1.ID.String(), e1JSON, nil},
 	}
 
 	for _, c := range cs {
@@ -57,6 +69,38 @@ func TestFetchSingleExerciseJSON(t *testing.T) {
 				assert.ErrorIs(t, err, c.err)
 			}
 			assert.Equal(t, c.expected, actual)
+		})
+	}
+}
+
+func TestStoreExerciseJSON(t *testing.T) {
+	e1 := Exercise{uuid.Nil, "New", 90.5, 6, uuid.Nil, uuid.Nil}
+	e1JSON, _ := json.Marshal(&e1)
+
+	e2 := Exercise{uuid.MustParse("5124a472-8315-4a0c-b9df-ed39bab3960c"), "Overwritten", 90.5, 6, uuid.Nil, uuid.Nil}
+	e2JSON, _ := json.Marshal(&e2)
+
+	e3 := Exercise{uuid.MustParse("6c255201-30dc-40c5-a016-b5374a7c4d6f"), "Error", 90.5, 6, uuid.Nil, uuid.Nil}
+	e3JSON, _ := json.Marshal(&e3)
+
+	xs := newTestExercises(e2)
+
+	cs := []struct {
+		testname string
+		given    []byte
+		expected error
+	}{
+		{"invalidJSON", []byte("{\"invalid\":true"), ErrInvalidJSON},
+		{"notAnExercise", []byte("{\"unknown\":1}"), ErrEmptyExercise},
+		{"newExercise", e1JSON, nil},                         // no id provided
+		{"existingExercise", e2JSON, nil},                    // existing id provided
+		{"notExistingExercise", e3JSON, ErrExerciseNotFound}, // id provided, but does not exist
+	}
+
+	for _, c := range cs {
+		t.Run(c.testname, func(t *testing.T) {
+			_, err := StoreExerciseJSON(&xs, c.given)
+			assert.ErrorIs(t, err, c.expected)
 		})
 	}
 }
